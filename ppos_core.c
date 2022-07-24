0c0623896da2e5dd5a7ecf817c79b24ced5e0972
tiagoserique <seriquevaladares29@gmail.com>
2022-07-24 01:17:21 -0300

    Add preemptive scheduling and time sharing

--- ppos_core.c
+++ ppos_core.c
@@ -5,6 +5,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/time.h>
 #include "ppos.h"
 #include "queue.h"
 
@@ -17,13 +19,23 @@ task_t *scheduler();
 // responsable for general control of the system
 void dispatcher();
 
+// the handler for the SIGALRM signal
+void tick_handler();
 
 // global variables ============================================================
 
 task_t MainTask, *CurrentTask, DispatcherTask, *ReadyQueue;
 
-int tid, userTask;
+int tid, userTask, quantum;
 
+// register the time elapsed since the first tick
+unsigned int system_time;
+
+// struct that defines a signal handler (must be global or static)
+struct sigaction action;
+
+// struct that defines the timer
+struct itimerval timer;
 
 // funções gerais ==============================================================
 
@@ -31,11 +43,33 @@ void ppos_init(){
     /* disable the stdout buffer, used by printf */
     setvbuf(stdout, 0, _IONBF, 0);
 
+    // register the SIGALRM signal handler
+    action.sa_handler = tick_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0 ;
+    if ( sigaction(SIGALRM, &action, 0) < 0 ){
+        perror("Erro em sigaction: ");
+        exit(1);
+    }
+
+    // ajust the timer to fire after 1ms
+    timer.it_value.tv_usec = 1000;      // first timer interrupt in ms
+    timer.it_value.tv_sec  = 0;         // first timer interrupt in s
+    timer.it_interval.tv_usec = 1000;   // recurring timer interrupt in ms
+    timer.it_interval.tv_sec  = 0;      // recurring timer interrupt in s
+
+    // set the ITIMER_REAL timer
+    if (setitimer(ITIMER_REAL, &timer, 0) < 0){
+        perror("ppos_init: set timer");
+        exit(1);
+    }
+
     // initialize global variables
     tid         = 0; 
     userTask    = 0;
     CurrentTask = NULL;
     ReadyQueue  = NULL;
+    quantum     = QUANTUM_DEFAULT;
 
     // create the main task
     MainTask.prev   = NULL;
@@ -44,6 +78,7 @@ void ppos_init(){
     MainTask.status = TASK_RUNNING;
     MainTask.static_prio  = DEFAUL_PRIO;
     MainTask.dynamic_prio = DEFAUL_PRIO;
+    MainTask.preemptable  = 0;
 
     // set main task as current task
     CurrentTask = &MainTask;
@@ -89,6 +124,10 @@ int task_create(task_t *task, void (*start_routine)(void *), void *arg){
     task->status = TASK_READY;
     task->static_prio  = DEFAUL_PRIO;
     task->dynamic_prio = DEFAUL_PRIO;
+    task->preemptable  = 0;
+    task->execution_time = systime();
+    task->processor_time = 0;
+    task->activations    = 0;
 
     // create the task context
     task->context.uc_stack.ss_sp    = stack;
@@ -104,9 +143,12 @@ int task_create(task_t *task, void (*start_routine)(void *), void *arg){
 
     // return the task's id if the id is from the main task or the dispatcher
     if ( tid <= 1 ) return task->id;
-    
+
     userTask++;
-    
+
+    // if is a user task, so it's preemptable
+    task->preemptable = 1;
+
     // add the task to the ready queue
     queue_append((queue_t **) &ReadyQueue, (queue_t *) task);
 
@@ -120,6 +162,9 @@ int task_switch(task_t *task){
         perror("task_switch: task is NULL");
         return -7;
     }
+
+    // increase the task's activations
+    task->activations++;
 
     task_t *cTask = CurrentTask; 
     CurrentTask   = task;
@@ -144,6 +189,9 @@ void task_exit(int exit_code){
     // set the task's status to finished
     cTask->status = TASK_FINISHED;
 
+    // calculate the task's execution time
+    cTask->execution_time = systime() - cTask->execution_time;
+
     // check if the task is the main task or the dispatcher
     // if it is not, decrease the number of user tasks
     if ( task_id() > 1 ) userTask--;
@@ -152,9 +200,15 @@ void task_exit(int exit_code){
     printf("task_exit: tarefa %d sendo encerrada\n", cTask->id);
     #endif
 
+    // output the task's information
+    printf("Task %d exit: execution time %d ms, processor time %d ms, %d activations\n", cTask->id, cTask->execution_time, cTask->processor_time, cTask->activations);
+
     // if the current task is the dispatcher, switch to the main task
     // otherwise, switch to the dispatcher task
-    ( cTask == &DispatcherTask ) ? task_switch(&MainTask) : task_switch(&DispatcherTask);
+    if ( cTask == &DispatcherTask )
+        task_switch(&MainTask);
+    else 
+        task_switch(&DispatcherTask);
 }
 
 
@@ -170,6 +224,28 @@ int task_id(){
 
 // operações de escalonamento ==================================================
 
+void tick_handler(){
+    task_t *cTask = CurrentTask;
+
+    // add the 1ms to the system time 
+    system_time++;
+
+    // check if the task is preemptable
+    if ( cTask->preemptable ){
+        quantum--;
+        
+        // check if the quantum is finished
+        if ( quantum == 0 ){
+
+            // reset the quantum
+            quantum = QUANTUM_DEFAULT;
+
+            task_yield();
+        }
+    }
+}
+
+
 task_t *scheduler(){
     task_t *taskMaxPriority, *firstTaskReady, *task;
     
@@ -181,12 +257,9 @@ task_t *scheduler(){
     
     // update the task's dynamic priority
     task = firstTaskReady->next;
-
     firstTaskReady->dynamic_prio -= TASK_AGING;
     while ( task != firstTaskReady ){
-        if ( task->dynamic_prio > MAX_PRIO ){
-            task->dynamic_prio -= TASK_AGING;
-        }
+        if ( task->dynamic_prio > MAX_PRIO ) task->dynamic_prio -= TASK_AGING;
         
         task = task->next;
     }
@@ -194,10 +267,10 @@ task_t *scheduler(){
     // search for the task with the highest priority
     taskMaxPriority = firstTaskReady;
     task = firstTaskReady->next;
-
     while ( task != firstTaskReady ){
-        if ( task->dynamic_prio <= taskMaxPriority->dynamic_prio )
+        if ( task->dynamic_prio < taskMaxPriority->dynamic_prio ){
             taskMaxPriority = task;
+        }
      
         task = task->next;
     }
@@ -225,8 +298,17 @@ void dispatcher(){
             // remove the task from the ready queue
             queue_remove((queue_t **) &ReadyQueue, (queue_t *) nextTask);
 
+            // get the system time before switch to the next task  
+            unsigned int prev_time = systime();
+
             // switch to the next task
             task_switch(nextTask);
+            
+            // get the total time that the task was running
+            prev_time = systime() - prev_time;
+            
+            // increase the task's processor time
+            nextTask->processor_time += prev_time;
 
             // handles all possible task status cases 
             switch ( nextTask->status ){
@@ -268,7 +350,8 @@ void task_setprio(task_t *task, int prio){
 
     // check if the task is null
     // if it is, set the task's priority to the current task
-    // otherwise, set the task's priority to the given task with the given priority
+    // otherwise, set the task's priority to the given task with the given 
+    // priority
     if ( !task ){
         cTask->static_prio  = prio;
         cTask->dynamic_prio = prio;
@@ -289,3 +372,9 @@ int task_getprio(task_t *task){
     return ( !task ) ? cTask->static_prio : task->static_prio;
 }
 
+// operações de gestão do tempo ================================================
+
+unsigned int systime(){
+    // return the system time
+    return system_time;
+}

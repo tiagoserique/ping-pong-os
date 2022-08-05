486a5ae396dcdd6cd002fe037cd21cf3b307f7ce
tiagoserique <seriquevaladares29@gmail.com>
2022-08-04 22:13:42 -0300

    Add the suspended queue and tasks

--- ppos_core.c
+++ ppos_core.c
@@ -22,10 +22,24 @@ void dispatcher();
 // the handler for the SIGALRM signal
 void tick_handler();
 
+// print the elements of the queue test
+void print_queue(void *element);
+
+// suspend the current task
+void task_suspend(task_t **queue);
+
+// resume the task that is suspended in the suspended queue 
+void task_resume(task_t * task, task_t **queue);
+
 // global variables ============================================================
 
-task_t MainTask, *CurrentTask, DispatcherTask, *ReadyQueue;
+// variables used to manage the tasks
+task_t mainTask, *currentTask, dispatcherTask;
 
+// queues used to manage the tasks
+task_t *readyQueue, *suspendedQueue;
+
+// tasks IDs, counter of user tasks and quantum counter
 int tid, userTask, quantum;
 
 // register the time elapsed since the first tick
@@ -36,6 +50,15 @@ struct sigaction action;
 
 // struct that defines the timer
 struct itimerval timer;
+
+// another functions ===========================================================
+
+void print_queue(void *element){
+    task_t *task = (task_t *) element;
+
+    printf("%d <- %d -> %d", task->prev->id, task->id, task->next->id);
+    task = task->next;
+}
 
 // funções gerais ==============================================================
 
@@ -65,28 +88,36 @@ void ppos_init(){
     }
 
     // initialize global variables
-    tid         = 0; 
-    userTask    = 0;
-    CurrentTask = NULL;
-    ReadyQueue  = NULL;
-    quantum     = QUANTUM_DEFAULT;
+    tid            = 0; 
+    userTask       = 1;
+    currentTask    = NULL;
+    readyQueue     = NULL;
+    quantum = QUANTUM_DEFAULT;
 
     // create the main task
-    MainTask.prev   = NULL;
-    MainTask.next   = NULL;
-    MainTask.id     = tid;
-    MainTask.status = TASK_RUNNING;
-    MainTask.static_prio  = DEFAUL_PRIO;
-    MainTask.dynamic_prio = DEFAUL_PRIO;
-    MainTask.preemptable  = 0;
+    mainTask.prev   = NULL;
+    mainTask.next   = NULL;
+    mainTask.id     = tid;
+    mainTask.status = TASK_READY;
+    mainTask.static_prio  = DEFAUL_PRIO;
+    mainTask.dynamic_prio = DEFAUL_PRIO;
+    mainTask.preemptable  = 1;
+    mainTask.execution_time = systime();
+    mainTask.processor_time = 0;
+    mainTask.activations    = 0;
+    mainTask.awake_time     = 0;
+
+    // append main task to the ready queue
+    queue_append((queue_t **) &readyQueue, (queue_t *) &mainTask);
 
     // set main task as current task
-    CurrentTask = &MainTask;
+    currentTask = &mainTask;
 
     // create the task for dispatcher
-    task_create(&DispatcherTask, dispatcher, NULL);
-}
+    task_create(&dispatcherTask, dispatcher, NULL);
 
+    task_yield();
+}
 
 // gerência de tarefas =========================================================
 
@@ -128,6 +159,7 @@ int task_create(task_t *task, void (*start_routine)(void *), void *arg){
     task->execution_time = systime();
     task->processor_time = 0;
     task->activations    = 0;
+    task->awake_time     = 0;
 
     // create the task context
     task->context.uc_stack.ss_sp    = stack;
@@ -137,20 +169,21 @@ int task_create(task_t *task, void (*start_routine)(void *), void *arg){
 
     makecontext(&(task->context), (void*)start_routine, 1, arg);
 
+
     #ifdef DEBUG
     printf("task_create: criou tarefa %d\n", task->id);
     #endif
 
-    // return the task's id if the id is from the main task or the dispatcher
-    if ( tid <= 1 ) return task->id;
 
+    // return the id if the id is from the dispatcher
+    if ( tid == 1 ) return task->id;
+
+
+    // if it is a user task, so increase the counter of user tasks, the task is 
+    // preemptable and add the task to the ready queue
     userTask++;
-
-    // if is a user task, so it's preemptable
     task->preemptable = 1;
-
-    // add the task to the ready queue
-    queue_append((queue_t **) &ReadyQueue, (queue_t *) task);
+    queue_append((queue_t **) &readyQueue, (queue_t *) task);
 
     return task->id;
 }
@@ -166,12 +199,14 @@ int task_switch(task_t *task){
     // increase the task's activations
     task->activations++;
 
-    task_t *cTask = CurrentTask; 
-    CurrentTask   = task;
+    task_t *cTask = currentTask; 
+    currentTask   = task;
+
 
     #ifdef DEBUG
     printf("task_switch: trocando contexto %d -> %d\n", cTask->id, task->id);
     #endif
+
 
     // switch the context
     if ( swapcontext(&(cTask->context), &(task->context)) < 0 ){
@@ -184,7 +219,7 @@ int task_switch(task_t *task){
 
 
 void task_exit(int exit_code){
-    task_t *cTask = CurrentTask;
+    task_t *cTask = currentTask;
 
     // set the task's status to finished
     cTask->status = TASK_FINISHED;
@@ -192,40 +227,89 @@ void task_exit(int exit_code){
     // calculate the task's execution time
     cTask->execution_time = systime() - cTask->execution_time;
 
-    // check if the task is the main task or the dispatcher
+    // check if the task is the dispatcher
     // if it is not, decrease the number of user tasks
-    if ( task_id() > 1 ) userTask--;
+    if ( task_id() != 1 ) userTask--;
+
+    // set the exit code of the task
+    cTask->exit_code = exit_code;
+
+    // remove the task from the suspended queue of the current task
+    #ifdef DEBUG
+    queue_print("task_exit: suspended queue", (queue_t *) cTask->suspendedQueue,
+     (void *) print_queue);
+    #endif
+
+    while ( cTask->suspendedQueue ){
+        task_resume((task_t *) cTask->suspendedQueue, (task_t **) &cTask->suspendedQueue);
+    }
+
 
     #ifdef DEBUG
     printf("task_exit: tarefa %d sendo encerrada\n", cTask->id);
     #endif
 
+
     // output the task's information
-    printf("Task %d exit: execution time %d ms, processor time %d ms, %d activations\n", cTask->id, cTask->execution_time, cTask->processor_time, cTask->activations);
+    printf("Task %d exit: execution time %d ms, processor time %d ms, %d activations\n", 
+    cTask->id, cTask->execution_time, cTask->processor_time, cTask->activations);
 
     // if the current task is the dispatcher, switch to the main task
     // otherwise, switch to the dispatcher task
-    if ( cTask == &DispatcherTask )
-        task_switch(&MainTask);
-    else 
-        task_switch(&DispatcherTask);
+    if ( cTask == &dispatcherTask ) task_switch(&mainTask);
+    else task_yield();
 }
 
 
 int task_id(){
     // check if the current task is null
-    if ( !CurrentTask ){
-        perror("task_id: CurrentTask is NULL");
+    if ( !currentTask ){
+        perror("task_id: currentTask is NULL");
         return -12;
     } 
 
-    return CurrentTask->id;
+    return currentTask->id;
 }
 
 // operações de escalonamento ==================================================
 
+void awake_tasks(){
+    // check if the queue is empty
+    if ( !suspendedQueue ) return;
+    
+    
+    #ifdef DEBUG
+        queue_print("awake_tasks: Suspended queue", (queue_t *) suspendedQueue, 
+                                                        (void *) print_queue);
+    #endif
+
+
+    // get the first task of the suspended queue
+    // get the time of the system
+    // compare the time of the system with the time of the task
+    // if the time of the system is greater than the time of the task,
+    // then the task is awake and it is added to the ready queue
+    task_t *cTask = suspendedQueue, *nextTask;
+    
+    unsigned int now = systime(); 
+    do {
+        // save the next task cause the current task could be removed from the 
+        // queue
+        nextTask = cTask->next;
+
+        if ( cTask->awake_time <= now ){
+            cTask->awake_time = 0;
+            task_resume((task_t *) cTask, (task_t **) &suspendedQueue);
+        }
+
+        // atualize the current task
+        cTask = nextTask;
+    } while ( suspendedQueue && nextTask != suspendedQueue );
+}
+
+
 void tick_handler(){
-    task_t *cTask = CurrentTask;
+    task_t *cTask = currentTask;
 
     // add the 1ms to the system time 
     system_time++;
@@ -238,13 +322,7 @@ void tick_handler(){
         quantum--;
         
         // check if the quantum is finished
-        if ( quantum == 0 ){
-
-            // reset the quantum
-            quantum = QUANTUM_DEFAULT;
-
-            task_yield();
-        }
+        if ( quantum == 0 ) task_yield();
     }
 }
 
@@ -253,7 +331,7 @@ task_t *scheduler(){
     task_t *taskMaxPriority, *firstTaskReady, *task;
     
     // get the first task ready on the queue
-    firstTaskReady = ReadyQueue;
+    firstTaskReady = readyQueue;
 
     // check if the queue is empty
     if ( !firstTaskReady ) return NULL;
@@ -288,27 +366,39 @@ task_t *scheduler(){
 void dispatcher(){
     // while there are user tasks 
     while ( userTask ){
+        
+        #ifdef DEBUG
+        queue_print("dispatcher: Ready queue", (queue_t *) readyQueue, 
+                                                        (void *) print_queue);
+        #endif
+
+        // awake the tasks that are sleeping
+        awake_tasks();
 
         // get the next task
         task_t *nextTask = scheduler();
 
-        // if the scheduler choose a task and it is not null
+        // if the scheduler choose a task and the task is not null
         if ( nextTask ){
 
-            // set status to running
+            // set status to running and remove the task from the ready queue
             nextTask->status = TASK_RUNNING;
-            
-            // remove the task from the ready queue
-            queue_remove((queue_t **) &ReadyQueue, (queue_t *) nextTask);
+            queue_remove((queue_t **) &readyQueue, (queue_t *) nextTask);
+
 
             // switch to the next task
             task_switch(nextTask);
-            
+
+            // reset the quantum
+            quantum = QUANTUM_DEFAULT;
+
+           
+
             // handles all possible task status cases 
             switch ( nextTask->status ){
                 case TASK_READY:
                     // add the task to the ready queue
-                    queue_append((queue_t **) &ReadyQueue, (queue_t *) nextTask);
+                    queue_append((queue_t **) &readyQueue, (queue_t *) nextTask);
                 break;
                 
                 case TASK_FINISHED:
@@ -325,27 +415,19 @@ void dispatcher(){
 
 
 void task_yield(){
-    task_t *cTask = CurrentTask;
-
-    // set the task's status to ready
-    cTask->status = TASK_READY;
-
     // pass the control to the dispatcher
-    task_switch(&DispatcherTask);
+    task_switch(&dispatcherTask);
 }
 
 
 void task_setprio(task_t *task, int prio){
-    task_t *cTask = CurrentTask;
+    task_t *cTask = currentTask;
 
     if ( prio > MIN_PRIO ) prio = MIN_PRIO;
 
-    if ( prio < MAX_PRIO ) prio = MAX_PRIO;
-
     // check if the task is null
     // if it is, set the task's priority to the current task
-    // otherwise, set the task's priority to the given task with the given 
-    // priority
+    // otherwise, set the task's priority to the given task
     if ( !task ){
         cTask->static_prio  = prio;
         cTask->dynamic_prio = prio;
@@ -358,7 +440,7 @@ void task_setprio(task_t *task, int prio){
 
 
 int task_getprio(task_t *task){
-    task_t *cTask = CurrentTask;
+    task_t *cTask = currentTask;
 
     // check if the task is null
     // if it is, return the current task's priority
@@ -366,9 +448,65 @@ int task_getprio(task_t *task){
     return ( !task ) ? cTask->static_prio : task->static_prio;
 }
 
+// operações de sincronização ==================================================
+
+void task_suspend(task_t **queue){
+    task_t *cTask = currentTask;
+
+    // remove the task from the ready queue
+    queue_remove((queue_t **) readyQueue, (queue_t *) cTask);
+
+    // set the task's status to suspended
+    cTask->status = TASK_SUSPENDED;
+
+    // append the task to the given queue
+    queue_append((queue_t **) queue, (queue_t *) cTask);
+
+    // pass the control to the dispatcher
+    task_yield();
+}
+
+
+void task_resume(task_t *task, task_t **queue){
+    // remove the task from the given queue
+    queue_remove((queue_t **) queue, (queue_t *) task);
+
+    // set the task's status to ready
+    task->status = TASK_READY;
+
+    // append the task to the ready queue
+    queue_append((queue_t **) &readyQueue, (queue_t *) task);
+}
+
+
+int task_join(task_t *task){
+    // check if the task is null or if the task was finished
+    if ( !task || task->status == TASK_FINISHED ){
+        #ifdef DEBUG
+        perror("task_join: task is NULL or is finished");
+        #endif
+        return -1;
+    }
+
+    // add the current task to the task's joined queue
+    task_suspend((task_t **) &task->suspendedQueue);
+
+    return task->exit_code;
+}
+
 // operações de gestão do tempo ================================================
+
+void task_sleep (int t){
+    task_t *cTask = currentTask;
+
+    // calculate the system time when the task will wake up and suspend it
+    cTask->awake_time = systime() + t;
+    task_suspend((task_t **) &suspendedQueue);
+}
+
 
 unsigned int systime(){
     // return the system time
     return system_time;
 }
+
--- ppos_data.h
+++ ppos_data.h
@@ -13,10 +13,10 @@
 
 #define STACKSIZE 64*1024
 
-#define TASK_READY    1
-#define TASK_RUNNING  2
-#define TASK_BLOCKED  3
-#define TASK_FINISHED 4
+#define TASK_READY     1
+#define TASK_RUNNING   2
+#define TASK_SUSPENDED 3
+#define TASK_FINISHED  4
 
 #define MIN_PRIO    20
 #define DEFAUL_PRIO 0
@@ -29,15 +29,23 @@
 typedef struct task_t
 {
     struct task_t *prev, *next;     // ponteiros para usar em filas
+    struct task_t *suspendedQueue;  // fila de tarefas suspensas
+    
     int id;				            // identificador da tarefa
     ucontext_t context;			    // contexto armazenado da tarefa
+    
     short status;			        // pronta, rodando, suspensa, ...
     short preemptable;			    // pode ser preemptada?
+    int exit_code;			        // valor de retorno da tarefa
+    
     short static_prio;              // prioridade estática
     short dynamic_prio;             // prioridade dinâmica
+    
     unsigned int execution_time;    // tempo de execução da tarefa
     unsigned int processor_time;    // tempo de processamento da tarefa
     unsigned int activations;       // número de vezes que a tarefa foi ativada
+
+    unsigned int awake_time;        // tempo em que a tarefa tem que ser acordada           
   // ... (outros campos serão adicionados mais tarde)
 } task_t ;
 
